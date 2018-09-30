bade3ef4369082e1601240996499f350289cfc7a
naoyahanai <regenesis.jp@gmail.com>
2018-10-01 01:19:00 +0900

    first

---
+++ Begin_RxSwift/ViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ViewModel.swift
+//  Begin_RxSwift
+//
+//  Created by naoya hanai on 2018/09/30.
+//  Copyright © 2018年 naoya hanai. All rights reserved.
+//
+
+import UIKit
+
+class ViewModel: NSObject {
+
+}
