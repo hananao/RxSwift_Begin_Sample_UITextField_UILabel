4642cd033f8cf0c6b7505defa6f74c344f4cf4de
naoyahanai <regenesis.jp@gmail.com>
2018-09-30 20:14:44 +0900

    Initial Commit

---
+++ Begin_RxSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Begin_RxSwift
+//
+//  Created by naoya hanai on 2018/09/30.
+//  Copyright © 2018年 naoya hanai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
